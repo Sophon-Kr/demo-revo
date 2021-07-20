abce77cb8cd88107b5d6c86422d162ffb87ce982
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2021-07-21 02:27:24 +0700

    initial structure in every file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import SignIn from "./views/SignIn";
+import SignUp from "./views/SignUp";
+import ForgotPass from "./views/ForgotPass";
+import Home from "./views/Home";
+import Profile from "./views/Profile";
+import UserList from "./views/UserList";
+import AddUser from "./views/AddUser";
+import EditUser from "./views/EditUser";
+import Task from "./views/Task";
+import ErrorPage from "./views/ErrorPage";
+import reducer from "./middleware/reducer";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 function App() {
+  const store = createStore(reducer, applyMiddleware(thunk));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/forgotpassword" component={ForgotPass} />
+            <Route exact path="/edit/:id" component={EditUser} />
+            <Route exact path="/add" component={AddUser} />
+            <Route exact path="/userlist" component={UserList} />
+            <Route exact path="/task" component={Task} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/" component={Home} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
